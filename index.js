a044edeb05f8bced6b13bab9f9da2fd33f54856e
TMinhHien <tminhhien@github.com>
2026-06-10 09:06:50 +0700

    fix: add default export to index.js for Expo Snack Device renderer

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// Expo Snack sử dụng App.js trực tiếp làm entry point
-// File này được giữ lại để tương thích với Expo CLI thông thường
-import { registerRootComponent } from 'expo';
 import App from './App';
 
-registerRootComponent(App);
+// Expo Snack (Device) yêu cầu default export từ index.js
+export default App;
